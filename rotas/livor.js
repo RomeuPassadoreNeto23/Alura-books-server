3d3c4992dd8fff52900db8421e1e7741c09ddc45
Romeu <passadore.neto@outlook.com>
2024-04-09 16:38:18 -0300

    Rotas de post, patch e delete

--- rotas/livor.js
+++ rotas/livor.js
@@ -6,4 +6,18 @@ router.get('/', (req, res) => {
     res.send("Olá Mundo")
 })
 
+router.post('/', (req, res) => {
+    res.send('você fez uma requisição do tipo POST')
+})
+
+router.delete('/', (req, res) => {
+    res.send('você fez uma requisição do tipo DELETE')
+})
+
+router.patch('/', (req, res) => {
+    res.send('você fez uma requisição do tipo PATCH')
+})
+
+
+
 module.exports = router
